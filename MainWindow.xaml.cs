75ec9b5c91560339a1e83a93884d8c178d638277
Striker1238 <stalker2404@list.ru>
2025-06-20 09:12:26 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace TestMaster
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
+        }
+    }
+}
